5a2ddc74188309b2d261f599cac46f422b390409
Pratham-Mishra04 <prathamm0409@gmail.com>
2025-02-04 08:07:18 +0530

    feat: minor fixes and additions

--- src/pages/admin/ended/index.tsx
+++ src/pages/admin/ended/index.tsx
@@ -39,7 +39,7 @@ const Index = () => {
     else getRounds();
   }, []);
 
-  const handleDownload = async (downloadType: 'team' | 'overall' | 'round', roundID?: string, roundIndex?: number) => {
+  const handleDownload = async (downloadType: 'team' | 'overall' | 'round' | 'prize', roundID?: string, roundIndex?: number) => {
     const toaster = Toaster.startLoad('Downloading CSV...');
     if (loading) return;
     setLoading(true);
@@ -65,6 +65,10 @@ const Index = () => {
             URL += `/rounds/${roundID}`;
             filename += `_round-${roundIndex + 1}-scores`;
           }
+          break;
+        case 'prize':
+          URL += '/prizes';
+          filename += '_prizes';
           break;
         default:
           isValid = false;
@@ -125,7 +129,7 @@ const Index = () => {
             <div className="--heading w-full h-full flex flex-col gap-8">
               <section className="w-full h-full text-center text-2xl md:text-3xl lg:text-5xl font-bold lg:leading-[4.5rem]">
                 <h1>
-                  The Hackathon has <span className={'text-4xl md:text-5xl gradient-text-3 lg:text-8xl block'}>Ended</span>
+                  The Hackathon has <span className={'text-4xl md:text-5xl gradient-text-3 lg:text-9xl block'}>Ended</span>
                 </h1>
               </section>
               <div className="w-full flex gap-4 justify-center">
@@ -134,60 +138,69 @@ const Index = () => {
                   triggerReload={announcementReloadTrigger}
                   triggerClass={'w-fit'}
                   trigger={
-                    <button className={'button-gradient rounded-lg h-full p-1 px-2 text-white'}>
-                      <HistoryIcon className={'size-5'} />
+                    <button className={'button-gradient rounded-lg h-full p-2 text-white'}>
+                      <HistoryIcon size={24} />
                     </button>
                   }
                 />
               </div>
-              {role == 'admin' && (
-                <div className="w-full flex flex-col gap-2">
-                  <div className="text-xl font-bold max-md:text-center">Event Reports (in CSV)</div>
-                  <div className="w-full flex gap-4 max-md:flex-col max-md:items-center relative">
-                    {loading && (
-                      <div className="w-full h-full bg-white flex-center absolute top-0 right-0 bg-opacity-50 rounded-lg">
-                        <Loader />
-                      </div>
-                    )}
-                    <Button
-                      onClick={() => handleDownload('team')}
-                      className="w-1/2 max-md:w-11/12 bg-blue-prime hover:bg-blue-prime/75 text-white"
-                      variant={'default'}
-                    >
-                      <div className="font-semibold">Team Details</div>
-                    </Button>
-                    <Button
-                      onClick={() => handleDownload('overall')}
-                      className="w-1/2 max-md:w-11/12 bg-blue-prime hover:bg-blue-prime/75 text-white"
-                      variant={'default'}
-                    >
-                      <div className="font-semibold">Overall Team Scores</div>
-                    </Button>
-                    {rounds && (
-                      <DropdownMenu>
-                        <DropdownMenuTrigger className="w-1/2 max-md:w-11/12">
-                          <Button
-                            onClick={() => handleDownload('round')}
-                            className="w-full bg-blue-prime hover:bg-blue-prime/75 text-white"
-                            variant={'default'}
-                          >
-                            <div className="font-semibold">Round Wise Team Scores</div>
-                          </Button>
-                        </DropdownMenuTrigger>
-                        <DropdownMenuContent>
-                          <DropdownMenuLabel>Select Round</DropdownMenuLabel>
-                          <DropdownMenuSeparator />
-                          {rounds.map(round => (
-                            <DropdownMenuItem key={round.id} onClick={() => handleDownload('round', round.id, round.index)}>
-                              Round {round.index + 1}
-                            </DropdownMenuItem>
-                          ))}
-                        </DropdownMenuContent>
-                      </DropdownMenu>
-                    )}
-                  </div>
+              <div className="w-full flex flex-col gap-2">
+                <div className="text-xl font-bold max-md:text-center">Event Reports (in CSV)</div>
+                <div className="w-full flex gap-4 max-md:flex-col max-md:items-center relative">
+                  {loading && (
+                    <div className="w-full h-full bg-white flex-center absolute top-0 right-0 bg-opacity-50 rounded-lg">
+                      <Loader />
+                    </div>
+                  )}
+                  <Button
+                    onClick={() => handleDownload('team')}
+                    className="w-1/2 max-md:w-11/12 bg-blue-prime hover:bg-blue-prime/75 text-white"
+                    variant={'default'}
+                  >
+                    <div className="font-semibold">Team Details</div>
+                  </Button>
+                  {role == 'admin' && (
+                    <>
+                      <Button
+                        onClick={() => handleDownload('overall')}
+                        className="w-1/2 max-md:w-11/12 bg-blue-prime hover:bg-blue-prime/75 text-white"
+                        variant={'default'}
+                      >
+                        <div className="font-semibold">Overall Team Scores</div>
+                      </Button>
+                      {rounds && (
+                        <DropdownMenu>
+                          <DropdownMenuTrigger className="w-1/2 max-md:w-11/12">
+                            <Button
+                              onClick={() => handleDownload('round')}
+                              className="w-full bg-blue-prime hover:bg-blue-prime/75 text-white"
+                              variant={'default'}
+                            >
+                              <div className="font-semibold">Round Wise Team Scores</div>
+                            </Button>
+                          </DropdownMenuTrigger>
+                          <DropdownMenuContent>
+                            <DropdownMenuLabel>Select Round</DropdownMenuLabel>
+                            <DropdownMenuSeparator />
+                            {rounds.map(round => (
+                              <DropdownMenuItem key={round.id} onClick={() => handleDownload('round', round.id, round.index)}>
+                                Round {round.index + 1}
+                              </DropdownMenuItem>
+                            ))}
+                          </DropdownMenuContent>
+                        </DropdownMenu>
+                      )}
+                      <Button
+                        onClick={() => handleDownload('prize')}
+                        className="w-1/2 max-md:w-11/12 bg-blue-prime hover:bg-blue-prime/75 text-white"
+                        variant={'default'}
+                      >
+                        <div className="font-semibold">Prize Distribution</div>
+                      </Button>
+                    </>
+                  )}
                 </div>
-              )}
+              </div>
             </div>
           </div>
         </div>
